e85f894fa3fa26270cd2b65d2ab1c4a772ced254
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-12-22 10:53:54 +0600

    Add banner section.

---
+++ src/components/Banner.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { motion } from "motion/react";
+import bannerImage from "../assets/banner.jpg";
+import petImage from "../assets/pet.svg";
+import documentImage from "../assets/document.svg";
+import phoneImage from "../assets/phone.svg";
+import walletImage from "../assets/wallet.svg";
+import { easeInOut } from "motion";
+
+const Banner = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${bannerImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        width: "100%",
+      }}
+    >
+      <div className="px-4 md:px-12 w-full min-h-screen flex flex-col-reverse md:flex-row items-center justify-center">
+        <div className="md:flex-1 space-y-4 mt-8 sm:mt-0">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+            Lost Something?
+          </h1>
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+            Found Something?
+          </h1>
+          <p className="text-xs">
+            Connect with your community to reunite items with their rightful
+            owners.
+          </p>
+          <button className="px-4 py-2 bg-sky-500 text-white font-bold hover:bg-sky-600 rounded-lg">
+            Learn More
+          </button>
+        </div>
+        <div className="flex items-center justify-center md:flex-1 my-8 md:my-0">
+          <motion.img
+            animate={{
+              x: [0, -10, 0],
+              transition: { repeat: Infinity, duration: 3, ease: easeInOut },
+            }}
+            className="w-16 h-16"
+            src={petImage}
+            alt="pet"
+          />
+          <div className="flex-col">
+            <motion.img
+              animate={{
+                y: [0, -10, 0],
+                transition: { repeat: Infinity, duration: 3, ease: easeInOut },
+              }}
+              className="w-16 h-16"
+              src={documentImage}
+              alt="document"
+            />
+            <motion.img
+              animate={{
+                y: [20, 30, 20],
+                transition: { repeat: Infinity, duration: 3, ease: easeInOut },
+              }}
+              className="w-16 h-16"
+              src={phoneImage}
+              alt="phone"
+            />
+          </div>
+          <motion.img
+            animate={{
+              x: [0, 10, 0],
+              transition: { repeat: Infinity, duration: 3, ease: easeInOut },
+            }}
+            className="w-16 h-16"
+            src={walletImage}
+            alt="wallet"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Banner from "../components/Banner";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default HomePage;
