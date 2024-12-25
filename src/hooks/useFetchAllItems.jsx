1daf4a7f8d95ae0e5e6fb850e0fa474243dc5eae
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-12-25 23:22:38 +0600

    Add pagination.

--- src/hooks/useFetchAllItems.jsx
+++ src/hooks/useFetchAllItems.jsx
@@ -3,23 +3,33 @@ import { AuthContext } from "../provider/AuthContext";
 import useAxios from "./useAxios";
 import { toast } from "react-toastify";
 
-const useFetchAllItems = () => {
-  const [data, setData] = useState([]);
+const useFetchAllItems = (setAllItems) => {
+  const [pgCnt, setPgCnt] = useState(0);
+  const [totalPageCount, setTotalPageCount] = useState(0);
   const { setDataLoading } = useContext(AuthContext);
   const axiosInstance = useAxios();
 
   useEffect(() => {
+    axiosInstance
+      .get("/totalData")
+      .then((res) => {
+        setTotalPageCount(Math.ceil(res?.data?.totalData) / 9);
+      })
+      .catch(() => cons.log("Something went wrong"));
+  }, []);
+
+  useEffect(() => {
     setDataLoading(true);
     axiosInstance
-      .get("/allItems")
+      .post("/allItems", { pgCnt: pgCnt * 9 })
       .then((res) => {
-        setData(res.data);
+        setAllItems(res.data);
       })
       .catch(() => toast.error("Something went wrong"))
       .finally(() => setDataLoading(false));
-  }, []);
+  }, [pgCnt]);
 
-  return data;
+  return { pgCnt, setPgCnt, totalPageCount };
 };
 
 export default useFetchAllItems;
--- src/pages/LostAndFoundItemsPage.jsx
+++ src/pages/LostAndFoundItemsPage.jsx
@@ -11,21 +11,22 @@ import { motion } from "motion/react";
 const LostAndFoundItemsPage = () => {
   const { dataLoading } = useContext(AuthContext);
   const [allItems, setAllItems] = useState([]);
-  const [key, setKey] = useState(null);
-  const fetchedData = useFetchAllItems();
+  const [key, setKey] = useState("");
+  const { pgCnt, setPgCnt, totalPageCount } = useFetchAllItems(setAllItems);
   const handleSearchItems = useSearchItem();
   const formatDate = useFomatDate();
 
   useEffect(() => {
-    setAllItems(fetchedData);
     const timerID = setTimeout(() => {
-      if (key !== null) {
+      if (key !== "") {
         handleSearchItems(key, setAllItems);
+      } else {
+        setPgCnt(0);
       }
     }, 1000);
 
     return () => clearTimeout(timerID);
-  }, [fetchedData, key]);
+  }, [key]);
 
   return (
     <div className="px-4 pb-8">
@@ -42,41 +43,62 @@ const LostAndFoundItemsPage = () => {
       ) : allItems.length === 0 ? (
         <NoDataFound />
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center">
-          {allItems.map((item) => (
-            <motion.div
-              key={item._id}
-              whileHover={{ scale: 1.01 }}
-              transition={{ duration: 0.3 }}
-              className="bg-gray-600 text-white w-full mx-auto p-4 max-w-80 space-y-3"
-            >
-              <h1 className=" text-xl truncate">{item?.title}</h1>
-              <img
-                className="w-full aspect-square"
-                src={item?.thumbnail}
-                alt="thumbnail"
-              />
-              <div className="flex justify-between">
-                <h1 className="text-xs">Post type: {item?.postType}</h1>
-                <h1 className="text-xs">Category: {item?.category}</h1>
-              </div>
-              <div className="flex justify-between">
-                <h1 className="text-xs">
-                  {item.postType === "lost" ? "Lost" : "Found"}:{" "}
-                  {formatDate(item?.date)}
-                </h1>
-                <h1 className="text-xs">Location: {item?.location}</h1>
-              </div>
-              <div>
-                <Link
-                  className="px-4 py-1 border border-white hover:text-gray-400"
-                  to={`/items/${item._id}`}
-                >
-                  View Details
-                </Link>
-              </div>
-            </motion.div>
-          ))}
+        <div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center">
+            {allItems.map((item) => (
+              <motion.div
+                key={item._id}
+                whileHover={{ scale: 1.01 }}
+                transition={{ duration: 0.3 }}
+                className="bg-gray-600 text-white w-full mx-auto p-4 max-w-80 space-y-3"
+              >
+                <h1 className=" text-xl truncate">{item?.title}</h1>
+                <img
+                  className="w-full aspect-square"
+                  src={item?.thumbnail}
+                  alt="thumbnail"
+                />
+                <div className="flex justify-between">
+                  <h1 className="text-xs">Post type: {item?.postType}</h1>
+                  <h1 className="text-xs">Category: {item?.category}</h1>
+                </div>
+                <div className="flex justify-between">
+                  <h1 className="text-xs">
+                    {item.postType === "lost" ? "Lost" : "Found"}:{" "}
+                    {formatDate(item?.date)}
+                  </h1>
+                  <h1 className="text-xs">Location: {item?.location}</h1>
+                </div>
+                <div>
+                  <Link
+                    className="px-4 py-1 border border-white hover:text-gray-400"
+                    to={`/items/${item._id}`}
+                  >
+                    View Details
+                  </Link>
+                </div>
+              </motion.div>
+            ))}
+          </div>
+          <div className="mt-8 w-full flex justify-center">
+            <div className="join">
+              <button
+                className="join-item btn"
+                onClick={() => setPgCnt((c) => (c > 0 ? c - 1 : 0))}
+              >
+                «
+              </button>
+              <button className="join-item btn">Page {pgCnt + 1}</button>
+              <button
+                className="join-item btn"
+                onClick={() =>
+                  setPgCnt((c) => (c + 1 < totalPageCount ? c + 1 : c))
+                }
+              >
+                »
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </div>
