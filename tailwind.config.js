82757b39adbb71eac3fdd29cfa108f266e7dbac3
Sakibur Rahman <sakib.cse.333@gmail.com>
2025-01-10 15:42:32 +0600

    Add custom color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "#16100d",
+        background: "#faf8f5",
+        primary: "#a17863",
+        secondary: "#c6d0af",
+        accent: "#a3bd8f",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
